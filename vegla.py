cdc32c3ef5240e2c5564c78ef38078ec1ffb9e13
Erion882 <erionramjan@gmail.com>
2025-02-03 12:28:20 +0100

    first commit

---
+++ vegla.py
@@ -0,0 +1,20 @@
+# programi per vendosjen e notes ne baze te pikeve
+
+piket = int(input("Numri i pikeve te provimit: "))
+if 0 <= piket < 50:
+    nota = 5
+elif 50 <= piket < 60:
+    nota = 6
+elif 60 <= piket < 70:
+    nota = 7
+elif 70 <= piket < 80:
+    nota = 8
+elif 80 <= piket < 90:
+    nota = 9
+elif 90 <= piket <= 100:
+    nota = 10
+else:
+    print("Piket e shenuara jane jo valide")
+    exit(0)
+
+print("Nota:", nota)
